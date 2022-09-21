3d9a907d83b466de5fb036037092e1323b31fce3
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-09-21 16:49:04 -0300

    create JS of pedido

---
+++ app/scripts/pedido.js
@@ -0,0 +1,87 @@
+let nome = document.getElementById("nome");
+let email = document.getElementById("email");
+let telefone = document.getElementById("telefone");
+let endereco = document.getElementById("endereco");
+
+let pequena = document.getElementById("p");
+let media = document.getElementById("m");
+let grande = document.getElementById("g");
+let gigante = document.getElementById("gg");
+
+let adicional = document.getElementById("borda");
+
+let refri = document.getElementById("refri");
+let suco = document.getElementById("suco");
+
+let entrega = document.getElementById("entrega");
+let retirada = document.getElementById("retirada");
+
+document.getElementById("confirmar").addEventListener("click", () => {
+  let tamanho;
+  let adicionais;
+  let bebida;
+  let tipoEntrega;
+
+  const pessoa = {
+    nome: nome.value,
+    email: email.value,
+    telefone: telefone.value,
+    endereco: endereco.value,
+  };
+
+  if (pequena.checked) {
+    tamanho = pequena.value;
+  } else if (media.checked) {
+    tamanho = media.value;
+  } else if (grande.checked) {
+    tamanho = grande.value;
+  } else if (gigante.checked) {
+    tamanho = gigante.value;
+  }
+
+  if (adicional.checked) {
+    adicionais = adicional.value;
+  }
+
+  if (refri.checked) {
+    bebida = refri.value;
+  } else if (suco.checked) {
+    bebida = suco.value;
+  }
+
+  if (entrega.checked) {
+    tipoEntrega = entrega.value;
+  } else if (retirada.checked) {
+    tipoEntrega = retirada.value;
+  }
+
+  const pizza = {
+    tamanho: tamanho,
+    adicionais: adicionais,
+    bebidas: bebida,
+    entrega: tipoEntrega,
+  };
+
+  //   console.log(nome.value);
+  //   console.log(email.value);
+  //   console.log(telefone.value);
+  //   console.log(endereco.value);
+
+  console.log(tamanho);
+  console.log(adicionais);
+  console.log(bebida);
+  console.log(tipoEntrega);
+
+  //   console.log(pequena);
+  //   console.log(media);
+  //   console.log(grande);
+  //   console.log(gigante);
+
+  //   console.log(adicional);
+
+  //   console.log(refri);
+  //   console.log(suco);
+
+  //   console.log(entrega);
+  //   console.log(retirada);
+});
