c780513882828d81ce303143d3d6cd82b6c5d223
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-09-22 11:32:33 -0300

    update

--- app/pages/pedido/index.js
+++ app/pages/pedido/index.js
@@ -16,23 +16,43 @@ let suco = document.getElementById("suco");
 let entrega = document.getElementById("entrega");
 let retirada = document.getElementById("retirada");
 
+while ((pequena.checked || media.checked || grande.checked || gigante.checked)) {
+  let tamanho;
+  if (pequena.checked) {
+    tamanho = 2;
+  } else if (media.checked) {
+    tamanho = 3;
+  } else if (grande.checked) {
+    tamanho = 4;
+  } else if (gigante.checked) {
+    tamanho = 5;
+  }
+
+  const selects = document.getElementById("selects")
+  for (let i = 0; i < tamanho; i++) {
+    let select = document.createElement("select");
+    selects.appendChild(select);
+  }
+}
+
+
 document.getElementById("confirmar").addEventListener("click", () => {
   let tamanho;
   let adicionais;
   let bebida;
   let tipoEntrega;
 
-  if (!(nome.value && email.value && telefone.value && endereco.value)) {
-    alert("Preencha todos os campos!");
-  } else {
-    const pessoa = {
-      nome: nome.value,
-      email: email.value,
-      telefone: telefone.value,
-      endereco: endereco.value,
-    };
-    // localStorage.setItem("pessoa", pessoa);
-  }
+  // if (!(nome.value && email.value && telefone.value && endereco.value)) {
+  //   alert("Preencha todos os campos!");
+  // } else {
+  //   const pessoa = {
+  //     nome: nome.value,
+  //     email: email.value,
+  //     telefone: telefone.value,
+  //     endereco: endereco.value,
+  //   };
+  //   // localStorage.setItem("pessoa", pessoa);
+  // }
 
   if (pequena.checked) {
     tamanho = pequena.value;
@@ -72,3 +92,5 @@ document.getElementById("confirmar").addEventListener("click", () => {
   // console.log(bebida);
   // console.log(tipoEntrega);
 });
+
+
